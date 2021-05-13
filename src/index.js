a6867a35e57f816ddd08cce60d566d743ff049f4
e-gunn <gunn816@hotmail.com>
2021-05-12 21:54:00 -0700

    Add array data model

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
+import { displayNames } from "./data.js";
 import "./styles.css";
 
-import "./styles.css";
+const renderNameTag = (name) => <NameTag name={name.name} key={name.id} />;
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Liz" />
-    <NameTag name="Erin" />
-    <NameTag name="Ben" />
-    <NameTag name="Mikey" />
-  </div>
-);
+const App = () => {
+  const NameTagElements = displayNames.map(renderNameTag);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
